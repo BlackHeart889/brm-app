b6ce6b290748a469e5a7d41bafca3c2d5f8c24a7
Cristhian Monrroy <monrroy88960@hotmail.com>
2023-09-27 02:09:29 -0500

    Eliminacion de funcionalidad innecesaria

--- controllers/auth/userController.js
+++
@@ -1,9 +0,0 @@
-exports.allAccess = (req, res) => {
-    res.status(200).send("Vista pública.");
-};
-exports.clienteBoard = (req, res) => {
-    res.status(200).send("Vista de cliente.");
-};
-exports.administradorBoard = (req, res) => {
-    res.status(200).send("Vista de administrador.");
-};
