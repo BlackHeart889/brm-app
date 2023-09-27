2d0ab519011015f305e2f7fc8cfcd64f9a4b2acd
Cristhian Monrroy <monrroy88960@hotmail.com>
2023-09-27 05:06:40 -0500

    Implementacion de variables de entorno

--- config/config.js
+++ config/config.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 module.exports = {
-    HOST: "localhost",
-    USER: "cmonrroy",
-    PASSWORD: "admin",
-    DB: "test-brm",
+    HOST: process.env.DB_HOST,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASSWORD,
+    DB: process.env.DB_NAME,
     dialect: "postgres",
     pool: {
         max: 5, //maxmimo de conexiones
